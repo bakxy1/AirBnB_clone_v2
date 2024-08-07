85a74879b0edb6c685a87855a54acf3262e5f748
Leykun Gizaw <leykungizaw@gmail.com>
2024-08-07 13:08:56 +0300

    fix(base_model): Update str and to_dict methods

--- models/base_model.py
+++ models/base_model.py
@@ -14,15 +14,16 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def __str__(self) -> str:
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self) -> None:
         self.updated_at = datetime.now()
 
     def to_dict(self) -> dict:
         instance_dict = deepcopy(self.__dict__)
+
         instance_dict["__class__"] = self.__class__.__name__
-        instance_dict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        instance_dict["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        instance_dict["created_at"] = self.created_at.isoformat()
+        instance_dict["updated_at"] = self.updated_at.isoformat()
 
         return instance_dict
