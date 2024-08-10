85011cd23e828d2d898faadb24d6caf2884bd4ee
Leykun Gizaw <leykungizaw@gmail.com>
2024-08-10 23:04:02 +0300

    feat(Classes): Add essential classes

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module defines `City` class"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class"""
+
+    name = ""
+    state_id = ""
