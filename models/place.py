8b491a8b0225b679453ba6ad3fed0c6c1434dab6
Leykun Gizaw <leykungizaw@gmail.com>
2024-08-16 19:43:06 +0300

    feat(Task 8): Add all necessary columns to Place

--- models/place.py
+++ models/place.py
@@ -2,19 +2,23 @@
 """Module defines `Place` class"""
 
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel):
     """Place class"""
 
-    name = ""
-    city_id = ""
-    user_id = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+
+    name = Column(String(128), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, default=0, nullable=False)
+    longitude = Column(Float, default=0, nullable=False)
     amenity_ids = []
